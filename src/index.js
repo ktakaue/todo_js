bf2841a7dc30a5643b99b2b7f5bee0fb9443571f
ktakaue <75979960+ktakaue@users.noreply.github.com>
2021-10-16 15:46:35 +0900

    戻す機能の実装とサンプルTodoの削除

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = " ";
 
+  createIncompleteList(inputText);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // div生成
   const div = document.createElement("div");
   div.className = "list-row";
@@ -13,7 +23,7 @@ const onClickAdd = () => {
   // liタグ生成
 
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
 
   // button完了タグを生成
   const completeButton = document.createElement("button");
@@ -37,6 +47,15 @@ const onClickAdd = () => {
     // buttonタグを生成
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグ（div）を完了リストから削除
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // テキストを取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
 
     // divタグの子要素に各要素を設定
     addTarget.appendChild(li);
@@ -61,12 +80,6 @@ const onClickAdd = () => {
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
 };
-
-// 未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
-};
-
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClickAdd());
